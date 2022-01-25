d81b1532519872a3cd3bc40bb24329fbb748985e
TerrenceArceo <76758014+TerrenceArceo@users.noreply.github.com>
2022-01-25 13:06:02 -0800

    created a useGenerateReport hook and transfered all logic code from GenerateReport component to useGenerateReport

---
+++ src/hooks/useGenerateReport.js
@@ -0,0 +1,42 @@
+import {useState, useEffect} from 'react'
+import { format } from 'date-fns'
+
+export default function useGenerateReport() {
+    const [reportType, setReportType] = useState('')
+    const [startDate, setStartDate] = useState(null)
+    const [endDate, setEndDate] = useState(null)
+
+    function handleChange(event) {
+        const { value } = event.target
+        setReportType(prevType => value)
+    }
+
+    function formattedDate(selectedDate) {
+        const newDate = new Date(selectedDate)
+        const formatted = format(newDate, 'yyyy-MM-dd')
+        return formatted
+    }
+
+    useEffect(() => {
+        if (startDate !== null && endDate !== null) {
+            fetch("https://viatouchmedia-test.apigee.net/loyalty/reports/sales", {
+                method: "POST",
+                body: JSON.stringify({
+                    "from_date": `${formattedDate(startDate)}`,
+                    "to_date": `${formattedDate(endDate)}`,
+                    "top_level_grouping": "client_id",
+                    "sort":"items_sold,d"
+                }),
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer YCXW1zkNJvg4T6aKK9W6sQx2bNrQ"
+                }
+            })
+                .then(res => res.json())
+                .then(data => console.log(data))
+        }
+    }, [startDate, endDate])
+
+
+    return { reportType, startDate, endDate, setStartDate, setEndDate, handleChange,  }
+}
--- src/subcomponents/GenerateReport.js
+++ src/subcomponents/GenerateReport.js
@@ -1,48 +1,9 @@
-import {useState, useEffect} from 'react'
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
-import { format } from 'date-fns'
+import useGenerateReport from '../hooks/useGenerateReport'
 
 export default function GenerateReport() {
-    const [reportType, setReportType] = useState('')
-    const [startDate, setStartDate] = useState(null)
-    const [endDate, setEndDate] = useState(null)
-
-
-    function handleChange(event) {
-        const { value } = event.target
-        setReportType(prevType => value)
-    }
-
-    function formattedDate(selectedDate) {
-        const newDate = new Date(selectedDate)
-        const formatted = format(newDate, 'yyyy-MM-dd')
-        return formatted
-    }
-
-    useEffect(() => {
-        if (startDate !== null && endDate !== null) {
-            fetch("https://viatouchmedia-test.apigee.net/loyalty/reports/sales", {
-                method: "POST",
-                body: JSON.stringify({
-                    "from_date": `${formattedDate(startDate)}`,
-                    "to_date": `${formattedDate(endDate)}`,
-                    "top_level_grouping": "client_id",
-                    "sort":"items_sold,d"
-                }),
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": "Bearer YCXW1zkNJvg4T6aKK9W6sQx2bNrQ"
-                }
-            })
-                .then(res => res.json())
-                .then(data => console.log(data))
-        }
-    }, [startDate, endDate])
-
-    // call the fetch inside the use effect
-    // use iff statement stating that if startDate and endDates are null
-
+    const { reportType, startDate, endDate, setStartDate, setEndDate, handleChange,  } = useGenerateReport()
 
     return (
         <section className='generate-report-component'>
